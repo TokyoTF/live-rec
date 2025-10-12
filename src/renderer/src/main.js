892bc648f2077ad71d70ceebffccacb0c74627a5
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2025-10-12 06:23:50 -0300

    Auto Migrate Svelte-5 CLI

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,8 +1,9 @@
 import './assets/main.css'
 
 import App from './App.svelte'
+import { mount } from "svelte";
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app')
 })
 
